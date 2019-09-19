685a3db8539c98c67f7ae50e147351dec3d47cd7
crazyones110 <44544415+crazyones110@users.noreply.github.com>
2019-09-19 15:53:51 +0800

    删除了登录测试的接口

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,30 +1,26 @@
 const { login } = require("../controller/user")
 const { SuccessModel, ErrorModel } = require("../model/resModel")
-
+const { set } = require("../db/redis")
 
 const handleUserRouter = (req, res) => {
     const {method, url} = req
     const path = url.split("?")[0]
 
-    if(method === "GET" && path === "/api/user/login") {
-        // const { username, password } = req.body
-        const { username, password } = req.query
+    if(method === "POST" && path === "/api/user/login") {
+        const { username, password } = req.body
+        // const { username, password } = req.query
         const loginResult = login(username, password)
         return loginResult.then(user => {
             if (user.username) {
                 // 设置 session
                 // 其实这一步有点多余，如果session里已经有了，就不需要赋值了
                 // 但好像不多余啊，因为再次访问登陆页面，只会退出登录了，然后cookie就被删除了
-                if (!req.session.username) {
-                    req.session.username = user.username
-                }
-                if (!req.session.realname) {
-                    req.session.realname = user.realname
-                }
-                // 因为已经 req.session = SESSION_DATA[userId],
-                // 所以更改 req.session 就是在更改 SESSION_DATA
-                
-                console.log("req.session  ", req.session)
+                // 其实不多余，因为有可能用户改用户名了啥的，所以这一步赋值是必须要有的
+                req.session.username = user.username
+                req.session.realname = user.realname
+                // 同步到 redis 中
+                set(req.sessionId, req.session)
+                console.log(3)
 
                 return new SuccessModel({
                     msg: "登录成功"
@@ -35,15 +31,16 @@ const handleUserRouter = (req, res) => {
         })
     }
 
-    if (method === "GET" && path === "/api/user/logintest") {
-        if (req.session.username) {
-            return Promise.resolve(new SuccessModel({
-                session: req.session
-            }))
-        } else {
-            return Promise.resolve(new ErrorModel("尚未登录"))
-        }
-    }
+    // if (method === "GET" && path === "/api/user/logintest") {
+    //     console.log("user.js中的req.session: ", req.session)
+    //     if (req.session.username) {
+    //         return Promise.resolve(new SuccessModel({
+    //             session: req.session
+    //         }))
+    //     } else {
+    //         return Promise.resolve(new ErrorModel("尚未登录"))
+    //     }
+    // }
 }
 
 module.exports = handleUserRouter
